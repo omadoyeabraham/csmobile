c83a8c6ff428c61ca5e465c4be63854075e7e79e
omadoyeabraham <omadoyeabraham@gmail.com>
2017-12-05 11:19:30 +0100

    save before installing ngrx

--- src/pages/stockbroking/stb-summary/stb-summary.ts
+++ src/pages/stockbroking/stb-summary/stb-summary.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// import { NgClass } from '@angular/common';
+import * as Highcharts from 'highcharts';
+
+// Services
+import { ChartsProvider } from '../../../providers/charts/charts';
 
 /**
  * Generated class for the StbSummaryPage page.
@@ -15,16 +20,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StbSummaryPage {
 
-  chart: string = "stockPerformance";
+  chartType: string = "portfolioPerformance";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public chartProvider: ChartsProvider) {
   }
 
   /**
-   * Ionic calls this function after this page is successfully loaded
+   * Ionic calls this function afters this page is successfully loaded
    */
   ionViewDidLoad() {
     // Initialize and draw the charts showing the user's portfolio performance and allocation
+
+    const portfolioAllocationChartObject = this.chartProvider.getPieChart([]);
+    const portfolioPerformanceChartObject = this.chartProvider.getBarChart([]);
+
+    Highcharts.chart('portfolioAllocationChart', portfolioAllocationChartObject);
+    Highcharts.chart('portfolioPerformanceChart', portfolioPerformanceChartObject);
+
   }
+
+  /**
+   * Determine whether or not to hide the portfolio performance chart based on what segment is selected
+   * This method was used because of the clash btw ionic's segment and highcharts (where charts become
+   * invisible after segments are toggled)
+   *
+   * @return boolean
+   */
+  hidePerformanceChart() {
+    return this.chartType === 'portfolioAllocation'
+  }
+
+  /**
+   * Determine whether or not to hide the portfolio performance chart based on what segment is selected
+   * This method was used because of the clash btw ionic's segment and highcharts (where charts become
+   * invisible after segments are toggled)
+   *
+   * @return boolean
+   */
+  hideAllocationChart() {
+    return this.chartType === 'portfolioPerformance'
+  }
+
 
 }
--- src/providers/charts/charts.ts
+++ src/providers/charts/charts.ts
@@ -15,4 +15,171 @@ export class ChartsProvider {
     console.log('Hello ChartsProvider Provider');
   }
 
+  /**
+   * Return the configuration, styling and data necessary to draw a piechart
+   *
+   * @param chartData
+   */
+  getPieChart(chartData: Array<any> = []) {
+    const chartObject = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      credits: {
+        enabled: false
+      },
+      legend: {
+        align: 'right',
+        verticalAlign: 'middle',
+        layout: 'vertical',
+        itemStyle: {
+          'fontWeight': 'normal'
+        }
+      },
+      title: {
+        text: ''
+      },
+      tooltip: {
+        pointFormat: '<b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          size: '100%',
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false,
+            formatter: function () {
+              return Math.round(this.percentage * 100) / 100 + ' %';
+            },
+            distance: -40,
+            rotation: 10
+          },
+          showInLegend: true
+        }
+      },
+      series: [{
+        name: 'SECTOR PERFORMANCE',
+        colorByPoint: true,
+        data: [
+          { name: 'IE', y: 56.33 },
+          { name: 'Chrome', y: 24.03 },
+          { name: 'Firefox', y: 10.38 },
+          { name: 'Safari', y: 4.77 },
+          { name: 'Opera', y: 0.91 },
+          { name: 'Other', y: 0.2 }
+        ]
+
+      }]
+    }
+
+    return chartObject
+  }
+
+  /**
+   * Return the configuration, styling and data necessary to draw a bar chart
+   *
+   * @param chartData
+   */
+  getBarChart(chartData: Array<any> = []) {
+    // Get the sector data for the portofolio and initialize variables
+    let dataSet = chartData
+    let graphData = [{
+      data: []
+    }]
+    let categories = []
+    let performanceColor = ''
+
+    /**
+     * Because we return when detecting portfolios with any holding data at all
+     */
+    if (dataSet === null) {
+      dataSet = []
+    }
+
+    dataSet.forEach((data) => {
+
+      // Red for losses and green for gains
+      performanceColor = (data.percentageGain < 0) ? '#FF0000' : '#00FF00'
+
+      // Format the data properly for display using highcharts column chart
+      graphData[0].data.push({
+        y: parseFloat(data.percentageGain),
+        color: performanceColor
+      })
+
+      categories.push(data.name)
+
+    })
+
+    const chartObject = {
+      chart: {
+        type: 'column',
+        verticalAlign: 'middle',
+        height: 190
+      },
+      title: {
+        text: ''
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0"> </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+      },
+      xAxis: {
+        categories: categories,
+      },
+      legend: {
+        enabled: false,
+        align: 'left',
+      },
+      yAxis: {
+        title: {
+          text: '( % )'
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      // series: graphData
+      series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+          name: 'Microsoft Internet Explorer',
+          y: 56.33,
+          drilldown: 'Microsoft Internet Explorer'
+        }, {
+          name: 'Chrome',
+          y: 24.03,
+          drilldown: 'Chrome'
+        }, {
+          name: 'Firefox',
+          y: 10.38,
+          drilldown: 'Firefox'
+        }, {
+          name: 'Safari',
+          y: 4.77,
+          drilldown: 'Safari'
+        }, {
+          name: 'Opera',
+          y: 0.91,
+          drilldown: 'Opera'
+        }, {
+          name: 'Proprietary or Undetectable',
+          y: 0.2,
+          drilldown: null
+        }]
+      }]
+    }
+
+    return chartObject;
+  }
+
 }
